b33ae08ba7e6eb0d4f12372ebe6e67f44d7888bb
guozebin <zebin.guo@live.com>
2017-08-24 10:08:52 +0800

    result 为 null 值，页面将不显示数据

--- Aqua/controllers/redis.js
+++ Aqua/controllers/redis.js
@@ -1,14 +1,18 @@
 var Redis = require('ioredis');
 var redis = new Redis();
 
-var fn_put = async function(ctx, next){
+var fn_put = async function (ctx, next) {
   let result = await redis.set(ctx.params.key, ctx.params.value);
   ctx.response.body = result;
 }
 
 
-var fn_get = async function(ctx, next){
+var fn_get = async function (ctx, next) {
   let result = await redis.get(ctx.params.key);
+  // 如果 result 为 null 值，页面将停留且显示旧数据
+  if (!result) {
+    result = "";
+  }
   ctx.response.body = result;
 }
 
